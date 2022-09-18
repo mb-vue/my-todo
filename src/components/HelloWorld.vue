e7a3de091a92bf0f15ed9a0c13203aff50be8ccc
rmasonh22 <106461878+rmasonh22@users.noreply.github.com>
2022-09-18 11:43:50 -0400

    use keycodes in input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,11 @@
 <template>
   <v-container padding>
   <!-- Remove old code -->
-  <input v-model="message"/>
+  <input v-model="message"
+  @keyup.esc="clearMessage()"
+  @keyup.enter="alertMessage()"
+  
+  />
   <button @click="message=clearMessage()">Clear</button>
   <h5>{{ message }}</h5>
 
@@ -25,9 +29,16 @@
     methods: {
       clearMessage() {
         this.message=''
+      },
+      alertMessage() {
+        alert(this.message)
       }
+
+     
+      }
+
     }
-  }
+  
 </script>
 
 <!-- Add style here -->
