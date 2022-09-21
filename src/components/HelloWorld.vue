7adc8ca262abeaa260e34e4f472a3bb8563a868f
rmasonh22 <106461878+rmasonh22@users.noreply.github.com>
2022-09-20 20:42:21 -0400

    adding counter -- check with console.log

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,9 @@
 <template>
   <v-container padding>
   <!-- Remove old code -->
+
+  <!--add counter-->
+ 
   <input v-model="message"
   @keyup.esc="clearMessage()"
   @keyup.enter="alertMessage()"
@@ -9,11 +12,19 @@
   
   />
   <button @click="message=clearMessage()">Clear</button>
+   <button
+    style="position: absolute; right: 10px; background-color: blue; color:white; width: 50px"
+    @click="counter++"
+  >{{ counter }}</button>
   <!-- <h5 class="border-grey" v-show="message.length">{{ message }}</h5> -->
   <!-- use v-if instead v-show-->
   <h5 class="border-grey" v-if="message.length">{{ message }}</h5>
   <!-- cnt cmd space to insert emoji-->
   <h3 v-else>No message entered 😎</h3>
+  <hr>
+  <p>Uppercase message: {{ messageUppercase() }}</p>
+
+
 
 
   </v-container>
@@ -28,7 +39,8 @@
     // create data object return some data 
     data() {
       return {
-        message: 'I love Jackie'
+        message: 'I love Jackie',
+        counter:0
       }
     },
     methods: {
@@ -37,6 +49,10 @@
       },
       alertMessage() {
         alert(this.message)
+      },
+      messageUppercase() {
+        console.log("This message is firing everytime I click button");
+        return this.message.toUpperCase()
       }
 
      
